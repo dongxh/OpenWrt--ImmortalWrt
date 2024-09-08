20c8745fe5ebde47888f40f67b77ae6bc4c61d76
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2024-09-08 16:35:44 +0800

    Update diy-part2-router.sh

--- diy-part2-router.sh
+++ diy-part2-router.sh
@@ -125,6 +125,18 @@ clean_packages package/custom
 # OpenClash
 git clone --single-branch --depth 1 -b master https://github.com/vernesong/OpenClash.git package/new/luci-app-openclash
 
+# 预置Clash内核
+echo -e "预置Clash内核"
+mkdir -p package//new/luci-app-openclash/root/etc/openclash/core
+core_path="package/new/luci-app-openclash/root/etc/openclash/core"
+goe_path="package//new/luci-app-openclash/root/etc/openclash"
+
+CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
+CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep "amd64" | awk -F '"' '{print $4}' | grep "v3" )
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+
 # golang
 rm -rf feeds/packages/lang/golang
 mv package/custom/golang feeds/packages/lang/
